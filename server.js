2e7c608f42c86db37bd35b4e727216c2dc6c267b
Jessica Palomo <jesicalynpalomo@gmail.com>
2020-11-22 13:43:38 -0500

    update server.js

--- server.js
+++ server.js
@@ -6,11 +6,59 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const cards = [
+  {
+    id: 1,
+    type: 'color',
+    value: 'red',
+    shape: 'heart'
+  },
+  {
+    id: 2,
+    type: 'color',
+    value: 'orange',
+    shape: 'diamond'
+  },
+  {
+    id: 3,
+    type: 'color',
+    value: 'yellow',
+    shape: 'star'
+  },
+  {
+    id: 4,
+    type: 'color',
+    value: 'green',
+    shape: 'triange'
+  },
+  {
+    id: 5,
+    type: 'color',
+    value: 'blue',
+    shape: 'circle'
+  },
+  {
+    id: 6,
+    type: 'color',
+    value: 'purple',
+    shape: 'square'
+  },
+  {
+    id: 7,
+    type: 'image',
+    value: 'URL',
+    shape: 'square'
+  }
+];
+
 // JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-app.get('/api/demo', (request, response) => {
-  response.json({
-    message: 'Hello from server.js'
-  });
+app.get('/api/draw/:id', (request, response) => {
+  try {
+    const { id } = request.params;
+    response.json(cards[id]);
+  } catch (e) {
+    response.status(500).json({ error: e.toString() });
+  }
 });
 // END DEMO
 
