5e1975b4816a5f3b54968790de5d7b742d69c817
Thip <thip.keovilay@gmail.com>
2020-11-28 18:18:56 -0500

    more updates with roll component

--- client/src/components/BoardImages.jsx
+++ client/src/components/BoardImages.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import wonkaheader from '../images/wonkaheader';
-import goldenticket from '../images/golden-ticket.png';
 
-<div>
-  <div className="cloudcontainer">
-    <div id="cloud-intro"></div>
-  </div>
-</div>;
+{
+  <div>
+    <div className="cloudcontainer">
+      <div id="cloud-intro"></div>
+    </div>
+  </div>;
+}
 
 <div>
   <div className="wonkaheader">
--- client/src/components/Roll.jsx
+++ client/src/components/Roll.jsx
@@ -1,11 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 import ReactDOM from 'react-dom';
-import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import goldenticket from '../images/golden-ticket.png';
-import heart from '../images/board/heart.png';
 import pushcandy from '../images/pushcandy.png';
 import agustuscard from '../images/cards/agustus-card.png';
 import blueberrycard from '../images/cards/blueberry-card.png';
@@ -20,65 +18,60 @@ import trianglecard from '../images/cards/triangle-card.png';
 import verucacard from '../images/cards/veruca-card.png';
 import willycard from '../images/cards/willy-card.png';
 
-// const Roll = () => {
-//   const {
-//     roll,
-//     setRoll,
-//     setPosition,
-//     position,
-//     positionSuit,
-//     setPositionSuit
-//   } = useContext(AppContext);
-//   const [image, getImage] = useState('');
+const Roll = () => {
+  const {
+    roll,
+    setRoll,
+    setPosition,
+    position,
+    positionSuit,
+    setPositionSuit
+  } = useContext(AppContext);
+  const [image, setImage] = useState('');
 
-//   const assignCard = (roll) => {
-//     switch (roll) {
-//       case 1:
-//         return heartcard;
-//         break;
-//       case 2:
-//         return diamondcard;
-//         break;
-//       case 3:
-//         return starcard;
-//         break;
-//       case 4:
-//         return trianglecard;
-//         break;
-//       case 5:
-//         return circlecard;
-//         break;
-//       case 6:
-//         return squarecard;
-//         break;
-//       case 7:
-//         return agustuscard;
-//         break;
-//       case 8:
-//         return blueberrycard;
-//         break;
-//       case 9:
-//         return verucacard;
-//         break;
-//       case 10:
-//         return mikecard;
-//         break;
-//       case 11:
-//         return charliecard;
-//         break;
-//       case 12:
-//         return willycard;
-//         break;
-//       default:
-//         return goldenticket;
-//     }
-//   };
-// };
-
-const Board = () => {
-  const { position, boardData, positionSuit, setPositionSuit } = useContext(
-    AppContext
-  );
+  const assignCard = (roll) => {
+    switch (roll) {
+      case 1:
+        return heartcard;
+        break;
+      case 2:
+        return diamondcard;
+        break;
+      case 3:
+        return starcard;
+        break;
+      case 4:
+        return trianglecard;
+        break;
+      case 5:
+        return circlecard;
+        break;
+      case 6:
+        return squarecard;
+        break;
+      case 7:
+        return agustuscard;
+        break;
+      case 8:
+        return blueberrycard;
+        break;
+      case 9:
+        return verucacard;
+        break;
+      case 10:
+        return mikecard;
+        break;
+      case 11:
+        return charliecard;
+        break;
+      case 12:
+        return willycard;
+        break;
+      default:
+        return goldenticket;
+    }
+    assignCard();
+  };
 
   useEffect(() => {
     // const randomIndex = Math.floor(Math.random() * 12 + 1);
@@ -94,16 +87,10 @@ const Board = () => {
       });
     }
 
-    document
-      .getElementById('button')
-      .addEventListener('click', function rollTheDice() {
-        alert('The form has been submitted!');
-      });
-
     // call function and capture return of the function to display cards randomly, fix line 31.
     let timerInterval;
     Swal.fire({
-      imageUrl: heart,
+      imageUrl: { roll },
       imageWidth: 200,
       imageHeight: 300,
       timer: 5000,
@@ -130,39 +117,28 @@ const Board = () => {
       }
     });
   }, [position]);
-};
-
-const Roll = () => {
-  const {
-    roll,
-    setRoll,
-    setPosition,
-    position,
-    positionSuit,
-    setPositionSuit
-  } = useContext(AppContext);
-  const [image, setImage] = useState('');
 
   const rollTheDice = async (e) => {
     e.preventDefault();
-    const randomNumber = Math.floor(Math.random() * 12);
+    const randomNumber = Math.floor(Math.random() * 12 + 1);
     setRoll(randomNumber);
 
     try {
       const { data } = await axios.get(`/api/draw/${randomNumber}`);
-      // console.log(data);
+      console.log(data);
       setPositionSuit(data.value);
       setPosition(Math.floor(data.id * 3.5) + 1);
       const displayCard = () => {
-        if (roll === 1) {
-          setImage(data.src);
-        } else {
-          setImage({ goldenticket });
-        }
+        //   if (roll === 1) {
+        //     setImage(data.src);
+        //   } else {
+        //     setImage({ goldenticket });
+        //   }
+        // };
+        displayCard();
+        console.log(data.name);
+        console.log(data.id);
       };
-      displayCard();
-      // console.log(data.name);
-      // console.log(data.id);
     } catch (error) {
       console.log(error);
     }
